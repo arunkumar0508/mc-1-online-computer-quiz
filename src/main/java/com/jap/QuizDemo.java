1e8c6f95f10cd66688ca2a0751e4c38974d2d0e2
ak8a66 <ak8a66@gmail.com>
2023-07-13 08:45:19 +0530

    done

--- src/main/java/com/jap/QuizDemo.java
+++ src/main/java/com/jap/QuizDemo.java
@@ -5,15 +5,42 @@ public class QuizDemo {
     //return the school name which has got the highest score in quiz
     //Handle the NumberFormatException
     public String highestScore(String []nameOfSchool , String[] scores ){
+        if(nameOfSchool==null || scores==null) {
+            return null;
+        }
 
+        int highestScore=0;
+        String schoolName=null;
+        try {
+            for (int i = 0; i < scores.length; i++) {
 
-        return "";
+                int score = Integer.parseInt(scores[i]);
+
+                if (score > highestScore) {
+                    highestScore = score;
+                    schoolName = nameOfSchool[i];
+                }
+            }
+        }catch (NumberFormatException obj){
+            return obj.toString();
+        }
+        return schoolName;
 
     }
     //convert all the names in uppercase
     //Handle the NullPointerException
     public String[] convertAllNamesToCapital(String name[]){
-        String upperCase[] = new String[0];
+        String upperCase[] = new String[5];
+        try {
+            for (int i = 0; i < name.length; i++) {
+                String names = name[i].toUpperCase();
+                upperCase[i]=names;
+            }
+        }catch (NullPointerException obj1){
+            String[] arr = new String[0];
+            arr[0]=obj1.toString();
+             return arr;
+        }
 
         return upperCase;
     }
